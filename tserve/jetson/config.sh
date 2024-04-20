7fb17bdc44de71c4f8219edd7ac894fcb25e20e0
Abdul Rehman <abdulrehmanee010@gmail.com>
2024-04-20 17:53:25 -0400

    bug-fix

--- tserve/jetson/config.sh
+++ tserve/jetson/config.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 source ../config.sh
+get_real_path() {
+    script_path=$(dirname $1)
+    script_path=$(realpath $script_path)
+}
+if [[ $0 != $BASH_SOURCE ]]; then
+    get_real_path $BASH_SOURCE
+else
+    get_real_path $0
+fi
+cd $script_path
 
 message() {
     cat <<\EOF
